7e6ecd31cb113f5c910655c3d99c23f08b077302
CharlMMZ <moyamarquezcarlos@gmail.com>
2024-09-25 12:35:27 +0200

    ejercicios clase 25/09/24 estructuras de control

--- 02_Estructuras_de_control/listas.php
+++ 02_Estructuras_de_control/listas.php
@@ -6,16 +6,18 @@
     <title>Document</title>
 </head>
 <body>
+    <h3>Lista con while</h3>
     <?php
         $i = 1;
         echo "<ul>";
-        while($i < 10){
+        while($i <= 10){
             echo "<li>$i</li>";
             $i++;
         }
         echo "</ul>"
 
     ?>
+    <h5>Forma alternativa</h5>
     <?php
         $i = 1;
         echo "<ul>";
@@ -25,6 +27,52 @@
             endif;
             $i++;
         endwhile;
+        echo "</ul>"
+    ?>
+    <h3>Lista con do while</h3>
+    <?php /*Do while */
+        $i = 1;
+        echo "<ul>";
+        do {
+            echo "<li>$i</li>";
+            $i++;
+        }while($i <= 10);
+        echo "</ul>"
+    ?>
+    <h3>Lista con for</h3>
+    <?php /*for */
+        $i = 1;
+        echo "<ul>";
+        for($i = 1; $i <= 10; $i++){
+            echo "<li>$i</li>";
+        }
+        echo "</ul>"
+    ?>
+    <h5>For incompletos</h5>
+    <?php /*for incompleto*/
+        $i = 1;
+        echo "<ul>";
+        for($i = 1; ;$i++){
+            if($i > 10) break;
+            echo "<li>$i</li>";
+        }
+        echo "</ul>";
+        /*For varias condiciones */
+        
+        $var = true;
+        echo "<ul>";
+        for($l = 1; $l <= 10 && $var == true; $l++){
+            echo "<li>$l</li>";
+        }
+        echo "</ul>";
+        /*Comillas sobles*/
+        $w=1;
+        echo "<ul>";
+        for(;;){
+            if($w > 10) break;
+            echo "<li>$w</li>";
+            $w++;
+        }
         echo "</ul>"
 
     ?>
