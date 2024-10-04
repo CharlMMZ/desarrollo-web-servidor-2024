f8e6035f90411797039470d7c9c00505935b7f60
CharlMMZ <moyamarquezcarlos@gmail.com>
2024-10-04 13:03:06 +0200

    web-servidor-03/10/2024

--- 03_Arrays/array_bidimensionales.php
+++ 03_Arrays/array_bidimensionales.php
@@ -29,22 +29,40 @@
             <th>Título</th>
             <th>Categoría</th>
             <th>Precio</th>
+            <th>¿Qué es?</th>
         </thead>
         <tbody>
             <?php 
             $nuevo_videojuego = ["Throne of Liberty", "MMO", 0];
             array_push($videojuegos,$nuevo_videojuego);
 
+            $nuevo_videojuego2 = ["Honkai Impact", "Mundo Abierto", 0];
+            array_push($videojuegos,$nuevo_videojuego2);
+
+            $nuevo_videojuego2 = ["Wuthering Waves", "Mundo Abierto", 0];
+            array_push($videojuegos,$nuevo_videojuego2);
             //Con 1, 0 o 2 podemos decirle que columna
             $_precio = array_column($videojuegos, 2);
             array_multisort($_precio, SORT_ASC, $videojuegos);
 
+
+            for ($i=0; $i < count($videojuegos); $i++) { 
+                if ($videojuegos[$i][2]<=0) {
+                    $videojuegos[$i][3] = "Gratis";
+                }else{
+                    $videojuegos[$i][3] = "De pago";
+                }
+            }
+
+
+
             foreach ($videojuegos as $videojuegui) {
-                list($titulillo, $categoria, $precio) = $videojuegui;
+                list($titulillo, $categoria, $precio, $es) = $videojuegui;
                 echo "<tr>";
                 echo "<td>$titulillo </td>";
                 echo "<td>$categoria </td>";
                 echo "<td>$precio </td>";
+                echo "<td>$es</td>";
                 echo "</tr>";
             }
             //para ordenar una matriz tenemos que descomponer en columnas
---
+++ 03_Arrays/notas.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php
+    error_reporting( E_ALL );
+    ini_set( "display_errors", 1 );
+    ?>
+</head>
+<body>
+<table>
+    <thead>
+        <th>Origen</th>
+        <th>Destino</th>
+        <th>Duración(min)</th>
+        <th>Precio</th>
+        <th>Tipo</th>
+    </thead>
+    <tbody>
+    <?php
+    $notas = [
+        ["Paco", "Desarrollo web servidor"],
+        ["Paco", "Desarrollo web cliente"],
+        ["Manu", "Desarrollo web servidor"],
+        ["Manu", "Desarrollo web cliente"]
+    ];
+
+    $nuevo_alumno = ["Jorgeta", "Desarrollo web servidor"];
+    array_push($notas,$nuevo_alumno);
+    $nuevo_alumno2 = ["Iker", "Diseño de interfaces web"];
+    array_push($notas,$nuevo_alumno2);
+    $nuevo_alumno3 = ["Jose Antonio", "Inglés"];
+    array_push($notas,$nuevo_alumno3);
+    $nuevo_alumno4 = ["Juan Antonio", "Diseño de interfaces web"];
+    array_push($notas,$nuevo_alumno4);
+
+    unset($notas[2]);
+
+    for ($i=0; $i < count($autobuses); $i++) { 
+        $autobuses[$i][2] = rand(0,10);
+        if ($autobuses[$i][2]<5) {
+            $autobuses[$i][3] = "NO APTO";
+        }else{
+            $autobuses[$i][3] = "APTO";
+        }
+    }
+    foreach ($autobuses as $busecitos) {
+        list($origen, $destino, $duracion, $precio, $X) = $busecitos;
+        echo "<tr>";
+        echo "<td>$origen </td>";
+        echo "<td>$destino </td>";
+        echo "<td>$duracion </td>";
+        echo "<td>$precio</td>";
+        echo "<td>$X</td>";
+        echo "</tr>";
+        
+    }
+
+
+    ?>
+    </tbody>
+</body>
+</html>
