2b384433f8913e5ff079ced0873395aec4497791
CharlMMZ <moyamarquezcarlos@gmail.com>
2024-10-23 13:07:50 +0200

    validacionFormularios

--- 04_Formularios/irpf.php
+++ 04_Formularios/irpf.php
@@ -4,59 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+    error_reporting( E_ALL );
+    ini_set( "display_errors", 1 );
+
+    require('../05_Funciones/salarios.php');
+    ?>
 </head>
 <body>
     <form action="" method="post">
         <input type="number" name="salario" placeholder="Salario">
+        <input type="hidden" name="accion" value="formulario_salario">
         <input type="submit" value="Calcular salario bruto">
+
     </form>
     <?php
     if($_SERVER["REQUEST_METHOD"] == "POST") {
-        $salario = $_POST["salario"];
+        if($_POST["accion"] == "formulario_salario") {
+            $tmp_salario = $_POST["salario"];
 
-        $salario_final = null;
+            if(filter_var($tmp_salario, FILTER_VALIDATE_FLOAT) === FALSE) {
+                echo "<p>Debe ser un número entero</p>";
+            } else {
+                if($tmp_base <= 0) {
+                    echo "<p>Debe ser mayor o igual que 0</p>";
+                } else {
+                    $salario = $tmp_salario;
+                    $res = calcularSalario($salario);
+                    
+                    echo "<p>El salario neto de $salario es $res</p>";
+                }
+            }
         
-        $tramo1 = (12450 * 0.19);
-        $tramo2 = ((20200 - 12450) * 0.24);
-        $tramo3 = ((35200 - 20200) * 0.30);
-        $tramo4 = ((60000 - 35200) * 0.37);
-        $tramo5 = ((300000 - 60000) * 0.45);
-
-        if($salario <= 12450) {
-            $salario_final = $salario - ($salario * 0.19);
-        } elseif ($salario > 12450 && $salario <= 20200) {
-            $salario_final = $salario 
-                - $tramo1 
-                - (($salario - 12450) * 0.24); 
-        } elseif ($salario > 20200 && $salario <= 35200) {
-            $salario_final = $salario
-                - $tramo1
-                - $tramo2
-                - (($salario - 20200) * 0.30);
-        } elseif ($salario > 35200 && $salario <= 60000) {
-            $salario_final = $salario 
-                - $tramo1
-                - $tramo2 
-                - $tramo3
-                - (($salario - 35200) * 0.37);
-        } elseif ($salario > 60000 && $salario <= 300000) {
-            $salario_final = $salario 
-                - $tramo1
-                - $tramo2 
-                - $tramo3
-                - $tramo4
-                - (($salario - 60000) * 0.45);
-        } else {
-            $salario_final = $salario
-                - $tramo1
-                - $tramo2 
-                - $tramo3
-                - $tramo4
-                - $tramo5
-                - (($salario - 300000) * 0.47);
-        }
-
-        echo "<h1>El salario neto de $salario es $salario_final</h1>";
     }
     ?>
 </body>
