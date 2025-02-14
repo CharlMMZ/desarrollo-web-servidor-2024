43ab683bc4b3bc1bbf0585e3cde56b048b410d6a
CharlMMZ <moyamarquezcarlos@gmail.com>
2025-02-14 12:36:35 +0100

    Laravel

---
+++ 08_Laravel/videojuegos/resources/views/consolas/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consolas</title>
+    <style>
+        li {
+            list-style: none;
+        }
+    </style>
+</head>
+<body>
+    <h1>Lista de Consolas</h1>
+    <ul>
+        @foreach ($consolas as $consola)
+            <li>{{$consola -> nombre}} {{$consola -> ano_lanzamiento}}</li>
+            
+        @endforeach
+    </ul>
+</body>
+</html>
